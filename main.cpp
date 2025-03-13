3ce92f8ad31ab0be422f1565c6be0546f617b1cf
varunkudan <varun@kudan.eu>
2025-03-13 11:43:07 +0100

    Added checks to dataset path and changed the name of the executable to estimate_scale

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 #include "libInertialScale.h"  // MATLAB-generated header
+#include <filesystem>
+#include <string>
+
+
+bool datasetInValid(const std::string& dataset)
+{
+    std::string path = "data/"+dataset;
+
+    if(!std::filesystem::is_directory(path))
+    {
+        std::cout<<"The dataset folder does not exits in data folder. Please check the name.\n"<<
+                    "The dataset folder should be placed in the path 'InertialScaleC-Matlab/data/' \n";
+        return true;
+    }
+
+    if(!std::filesystem::exists(path+"poses.txt"))
+    {
+        std::cout<<"The dataset folder does not contain poses.txt. Please add the poses.txt file\n";
+        return true;
+    }
+
+    if(!std::filesystem::exists(path+"accelerometer.txt"))
+    {
+        std::cout<<"The dataset folder does not contain accelerometer.txt. Please add the poses.txt file\n";
+        return true;
+    }
+
+    if(!std::filesystem::exists(path+"gyroscope.txt"))
+    {
+        std::cout<<"The dataset folder does not contain gyroscope.txt. Please add the poses.txt file\n";
+        return true;
+    }
+
+    return false;
+}
 
 int main(int argc, char** argv) {
+    if(argc < 2)
+    {
+        std::cerr<<"Please enter the name of a valid dataset\n";
+        return 1;
+    }
+
+    if(datasetInValid(argv[1]))
+    {
+        return 1;
+    }
+
     // Initialize MATLAB Runtime
     if (!libInertialScaleInitialize()) {
         std::cerr << "Failed to initialize MATLAB Runtime." << std::endl;
